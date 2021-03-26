0407d996aee5951973a83be54280934e518f159d
snowlittlelamb <81389463+snowlittlelamb@users.noreply.github.com>
2021-03-25 22:34:19 -0400

    Make CVS searhable at town level

--- cvs_vac.py
+++ cvs_vac.py
@@ -20,33 +20,32 @@ def check_state(state):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
 
-    status_dict = dict()
-
+    columns = []
+    data = []
     for table in soup.find_all('table'):
-        for row in table.tbody.find_all('tr'):
-            city = None
-            status = None
-            for cell in row.find_all('td'):
-                span = cell.find('span')
-                if 'status' in span.attrs.get('class'):
-                    status = span.text
-                else:
-                    city = span.text
-            if city is not None and status is not None:
-                status_dict.update({city: status})
+        columns = [th.text for th in table.thead.find_all('th')]
+        for tr in table.tbody.find_all('tr'):
+            data.append([td.text for td in tr.find_all('td')])
 
-    status_df = pd.Series(status_dict)
-    avail_cities = status_df[status_df != 'Fully Booked']
+    status_df = pd.DataFrame(data, columns=columns)
+    avail_cities = status_df[status_df[columns[-1]] != 'Fully Booked']
     return avail_cities, status_df
 
 
 state= input("The state you are checking, with the first letter capitalized: ")
+input_towns = input("Enter the towns you are checking (for example: New York, NY; Albany, NY)")
 sender = input("Sender email: ")
 password = input("Sender email password: ")
 receiver = input("Receiver email: ")
 
 # Port for SSL
 port = 465
+
+towns = input_towns.split(";") if input_towns is not None else None
+def clean_up(town):
+    city, _, st = town.rpartition(",")
+    return city.strip().lower() + "," + st.rstrip()
+towns = [clean_up(town) for town in towns] if towns is not None else None
 
 def check_and_send():
     # How often to refresh the page, in seconds
@@ -58,12 +57,13 @@ def check_and_send():
     threading.Timer(update, check_and_send).start()
 
     avail, _ = check_state(state)
+    avail_loc = avail.loc[avail[avail.columns[0]].isin(towns), avail.columns[0]] if towns is not None else avail[avail.columns[0]]
 
     if len(avail.index) > 0:
 
         print(f"Available Appointment found in {state}; sending emails!")
         # Message in the email.
-        cities ="\n".join(avail.index)
+        cities ="\n".join(avail_loc.index)
         message = f"Subject:CVS Vaccine Availability\n\nAvailable Cities in {state}:\n{cities}"
 
         # Create a secure SSL context
